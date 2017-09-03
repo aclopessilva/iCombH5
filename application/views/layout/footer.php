bc5d0e323cc91c4402013563156ac6e2a7e67eae
Ana <ana.lopsil@gmail.com>
2017-09-02 22:08:38 -0300

    tiramos uma dependencia desnecessaria

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,6 +1,5 @@
     <script src="<?=base_url()?>user_guide/vendor/jquery/jquery.min.js"></script>
     <script src="<?=base_url()?>user_guide/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?=base_url()?>user_guide/js/contact_me.js"></script>
     <script src="<?=base_url()?>user_guide/js/agency.min.js"></script>
 </body>
 
