5f90e6d530b9b1a17d07ad31e23839bc48b56fdd
Ana <ana.lopsil@gmail.com>
2017-10-23 23:33:48 -0200

    corrigindo botao de "clique aqui para ver indicadores"

--- application/views/exercicio/exercicio.php
+++ application/views/exercicio/exercicio.php
@@ -1006,7 +1006,7 @@
                 success: function(data){
                     if(data.estado == 'OK'){
 
-                        modalResultadoExercicio('Resultado final',"<a class=\"btn btn-primary\"  href='<?= base_url()?>indicador'>Clique aqui</a>");
+                        modalResultadoExercicio('Resultado final',"<a class=\"btn btn-primary\"  href='<?= base_url()?>indicador'>Clique aqui para ver os detalhes.</a>");
 
                     }else{
                         alert(data.mensagem);
@@ -1032,7 +1032,7 @@
         html += '</div>';
         html += '<div class="modal-body">';
         html += '<div class="BtnIndicadorAlign">'+formContent;
-        html += ' para ver os detalhes. </div>';        
+        html += '</div>';        
         html += '<div class="modal-header">';
         html += '</div>';
         html += '<div class="modal-footer">';
